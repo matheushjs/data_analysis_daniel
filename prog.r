8af7656b2701936c9d11dff0a24eb21dfd1c6a4d
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-10-01 09:34:00 -0300

    Add lm models

--- prog.r
+++ prog.r
@@ -16,3 +16,16 @@ d2 = as.matrix(d) %*% vec
 plot3d(d2, col=as.integer(data$"Income.group"), size=10)
 
 plot3d(d, col=as.integer(data$"Income.group"), size=10)
+
+model = lm(d[,1] ~ d[,2])
+print(summary(model));
+
+model = lm(d[,1] ~ d[,2] + d[,3])
+print(summary(model));
+
+llog = log(d[,3]);
+model = lm(d[,1] ~ llog)
+print(summary(model));
+
+model = lm(d[,1] ~ d[,2] + llog)
+print(summary(model));
